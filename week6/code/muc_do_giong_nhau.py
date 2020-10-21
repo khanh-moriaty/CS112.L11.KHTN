dce23c824ca36de608be8b2688a4bcc5361ba8ed
Khanh Moriaty <19520624@gm.uit.edu.vn>
2020-10-22 01:46:09 +0700

    Uploaded docs for week6's muc_do_giong_nhau

--- week6/code/muc_do_giong_nhau.py
+++ week6/code/muc_do_giong_nhau.py
@@ -1,3 +1,3 @@
 s, t = input(), input()
-a = {x+y for x,y in zip(t[:-1], t[1:])}
-print(sum([x+y in a for x,y in zip(s[:-1], s[1:])]))
+a = {x+y for x,y in zip(t, t[1:])}
+print(sum([x+y in a for x,y in zip(s, s[1:])]))
